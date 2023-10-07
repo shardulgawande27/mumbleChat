537ae8bbaadfe343934ac69de93392f7c016f644
Shardul Gawande <shardulgawaned27@gmail.com>
2023-10-07 18:45:36 +0530

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,15 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "chat-ab746.firebaseapp.com",
-  projectId: "chat-ab746",
-  storageBucket: "chat-ab746.appspot.com",
-  messagingSenderId: "901216368405",
-  appId: "1:901216368405:web:8ec942ee51611df5c49b1c",
+  apiKey: "AIzaSyDvF2tIZekCFcXN-xp3euifQFVKC_dOMOM",
+  authDomain: "mumblechat-66650.firebaseapp.com",
+  projectId: "mumblechat-66650",
+  storageBucket: "mumblechat-66650.appspot.com",
+  messagingSenderId: "1048236620276",
+  appId: "1:1048236620276:web:1e53ca9a81a637c73561a1",
 };
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
-export const db = getFirestore()
+export const db = getFirestore();
